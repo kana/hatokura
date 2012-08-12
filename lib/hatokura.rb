fe9e814fdeeee4b0312195a06dc5d67e6754b979
Kana Natsuno <dev@whileimautomaton.net>
2012-08-12 13:51:21 +0900

    Make all submodules autoloaded

--- lib/hatokura.rb
+++ lib/hatokura.rb
@@ -1,5 +1,16 @@
-require "hatokura/version"
-
 module Hatokura
-  # Your code goes here...
+  class << self
+    def file_name_from_identifier(identifier)
+      identifier.
+        to_s().
+        gsub(/[[:upper:]]+/) {|us| '_' + us.downcase()}.
+        sub(/^_/, '')
+    end
+  end
+
+  [
+    :VERSION,
+  ].each do |identifier|
+    autoload identifier, "hatokura/#{file_name_from_identifier(identifier)}"
+  end
 end
