cf41323bb095f1df3f467cc5fc6a35104012b75c
Kana Natsuno <dev@whileimautomaton.net>
2012-08-30 20:23:39 +0900

    Add spec for Graveyard

---
+++ spec/graveyard_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Hatokura
+  describe Graveyard do
+    zone = described_class.new(Player.new())
+
+    it_behaves_like 'public zone', zone
+    it_behaves_like 'owned zone', zone
+
+    context 'by default' do
+      it_behaves_like 'empty zone', zone
+    end
+  end
+end
