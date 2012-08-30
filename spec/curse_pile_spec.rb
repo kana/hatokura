0e0dfc7fc3e7b3b7387af5fa0299cd8aaa2529e1
Kana Natsuno <dev@whileimautomaton.net>
2012-08-30 20:24:54 +0900

    Add spec for CursePile

---
+++ spec/curse_pile_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Hatokura
+  describe CursePile do
+    zone = described_class.new()
+
+    it_behaves_like 'public zone', zone
+    it_behaves_like 'shared zone', zone
+
+    context 'by default' do
+      it_behaves_like 'empty zone', zone
+    end
+  end
+end
