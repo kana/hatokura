bd2da986938965beec448243720ee34f2cb139e6
Kana Natsuno <dev@whileimautomaton.net>
2012-08-30 20:22:06 +0900

    Add spec for Hand

---
+++ spec/hand_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Hatokura
+  describe Hand do
+    zone = described_class.new(Player.new())
+
+    it_behaves_like 'private zone', zone
+    it_behaves_like 'owned zone', zone
+
+    context 'by default' do
+      it_behaves_like 'empty zone', zone
+    end
+  end
+end
