0c4e92df29e4d85e5c32366b62f07df7938d8bfe
Kana Natsuno <dev@whileimautomaton.net>
2014-06-11 00:24:13 +0900

    Autoload this gem's files

--- lib/hatokura.rb
+++ lib/hatokura.rb
@@ -2,5 +2,17 @@ require 'bundler/setup'
 require "hatokura/version"
 
 module Hatokura
-  # Your code goes here...
+  class << self
+    def const_missing(identifier)
+      require "hatokura/#{file_name_from_identifier(identifier)}"
+      eval identifier.to_s()
+    end
+
+    def file_name_from_identifier(identifier)
+      identifier
+        .to_s()
+        .gsub(/[[:upper:]]+/) {|us| '_' + us.downcase()}.
+        sub(/^_/, '')
+    end
+  end
 end
