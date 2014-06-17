d7c93d838dabb1caad4c098a24eb3ac36f59e545
Kana Natsuno <dev@whileimautomaton.net>
2014-06-18 00:16:29 +0900

    Define NoviceMaid prototype

---
+++ lib/hatokura/fairy_garden.rb
@@ -0,0 +1,13 @@
+module Hatokura
+  module FairyGarden
+    class NoviceMaid
+      def initialize()
+        @name = '見習い侍女'
+        @cost = 2
+        @link = nil
+        @main_type = :authority
+        @authority_point = -2
+      end
+    end
+  end
+end
