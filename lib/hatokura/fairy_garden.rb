49467a89cbf6ab14e90caa407089aa877ae51119
Kana Natsuno <dev@whileimautomaton.net>
2014-06-18 00:18:42 +0900

    Define FarmVillage prototype

--- lib/hatokura/fairy_garden.rb
+++ lib/hatokura/fairy_garden.rb
@@ -1,5 +1,16 @@
 module Hatokura
   module FairyGarden
+    class FarmVillage
+      def initialize()
+        @name = '農村'
+        @cost = 1
+        @link = 1
+        @main_type = :land
+        @authority_point = -2
+        @played_ability = Proc.new {[]}  # TODO: List moves.
+      end
+    end
+
     class NoviceMaid
       def initialize()
         @name = '見習い侍女'
