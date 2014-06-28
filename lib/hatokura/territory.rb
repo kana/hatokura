c785426928703a38e2b42307cecc898217d343f8
Kana Natsuno <dev@whileimautomaton.net>
2014-06-28 15:46:52 +0900

    Define Territory

---
+++ lib/hatokura/territory.rb
@@ -0,0 +1,12 @@
+module Hatokura
+  # As a concept, Territory does not exist until its owner backs up a princess.
+  # But we don't have to implement as is.
+  #
+  # Original term: 直轄地
+  class Territory
+    def initialize()
+      super
+      @publicp = true
+    end
+  end
+end
