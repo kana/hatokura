37ed45b1684a2748375c36c00411272c27fb1dab
Kana Natsuno <dev@whileimautomaton.net>
2014-06-28 15:35:18 +0900

    Define Field

---
+++ lib/hatokura/field.rb
@@ -0,0 +1,11 @@
+module Hatokura
+  # Note that each player has his or her own Field.
+  #
+  # Original term: 場
+  class Field
+    def initialize()
+      super
+      @publicp = true
+    end
+  end
+end
