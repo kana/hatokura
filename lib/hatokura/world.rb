150cc9aff967880d24b84d4574a673b86618d9bd
Kana Natsuno <dev@whileimautomaton.net>
2014-06-29 19:22:08 +0900

    Define World skeleton

---
+++ lib/hatokura/world.rb
@@ -0,0 +1,20 @@
+module Hatokura
+  # Represents a snapshot of a game.
+  class World
+    # TODO: Define shared zones:
+    # プリンセスカード置き場
+    # 呪い置き場
+    # 不運置き場
+    # サプライ
+    # ランダムマーケット
+    # ベーシックマーケット
+    # 国外
+    #
+    # TODO: Define owned zones:
+    # 手札
+    # 山札
+    # 捨て札置き場
+    # 直轄地 (this does not exist until its owner backs up a princess.)
+    # 場
+  end
+end
